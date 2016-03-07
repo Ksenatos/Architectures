a82f0f3d53edcd25167d6e2d237c8fc84d4a561c
Ksenatos <naboychenko.p@gmail.com>
2016-03-07 14:02:17 +0200

    вот наши рабочие файлики

---
+++ controller.py
@@ -0,0 +1,36 @@
+__author__ = 'paul'
+from model import DB
+from view import View
+class Controller:
+    def __init__(self):
+        self.view = View()
+        self.mydb = DB()
+        self.choises = {1: self.Show_books, 2: self.Show_authors, 3: self.Add_book, 4: self.Add_author, 5: self.Quit}
+    def run(self):
+        while True:
+            self.view.display_menu()
+            choices = input("Enter an option: ")
+            action = self.choises.get(choices)
+            if action:
+                action()
+            else:
+                print("{0} is not a valid choice".format(choices))
+    def Show_books(self):
+        for i in self.mydb.get_books():
+            print(i)
+    def Show_authors(self):
+        for i in self.mydb.get_authors():
+            print(i)
+
+    def Add_book(self):
+        books_name, authors_id = input('Enter books_name and authors_id: ')
+        self.mydb.add_book(books_name, authors_id)
+    def Add_author(self):
+        authors_name, authors_lastname, authors_age = input('Enter authors name, authors last name, authors age: ')
+        self.mydb.add_book(authors_name, authors_lastname, authors_age)
+    def Quit(self):
+        print("Bye")
+        quit()
+
+if __name__ == "__main__":
+    Controller().run()
---
+++ view.py
@@ -0,0 +1,12 @@
+__author__ = 'paul'
+#from controller import Controller
+
+class View:
+    def display_menu(self):
+        print(""" Main menu
+        1. Show books
+        2. Show authors
+        3. Add book
+        4. Add author
+        5. Quit
+        """)
